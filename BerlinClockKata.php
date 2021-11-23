65765ce31cdf709ab31ca33a848e21daa08508b6
unknown <abdenour.didi@student.vinci.be>
2021-11-23 18:20:28 +0100

    insert - method getMinute & Test testMinutesGiven1

--- BerlinClockKata.php
+++ BerlinClockKata.php
@@ -1,6 +1,34 @@
 <?php
 namespace BerlinClockKata;
 
-class BerlinClockKata {
+class BerlinClockKata
+{
 
+    public function getMinute($minutes)
+    {
+        $nombreDeMinuteDuHaut = $minutes / 5;
+        $nombreDeMinuteDuBas = $minutes % 5;
+        $string = "";
+        for ($i = 0; $i < 11; $i++) {
+            if ($nombreDeMinuteDuHaut >=1) {
+                $string .= "Y";
+                $nombreDeMinuteDuHaut--;
+            } else {
+                $string .= "O";
+            }
+        }
+
+        $string .= " ";
+
+        for ($i = 0; $i < 4; $i++) {
+            if ($nombreDeMinuteDuBas >= 1) {
+                $string .= "Y";
+                $nombreDeMinuteDuBas--;
+            } else {
+                $string .= "O";
+            }
+        }
+
+        return $string;
+    }
 }
--- BerlinClockKataTest.php
+++ BerlinClockKataTest.php
@@ -1,8 +1,14 @@
 <?php
 namespace BerlinClockKata;
+use function PHPUnit\Framework\assertEquals;
+
 require("BerlinClockKata.php");
 
-class BerlinClockKataTest extends PHPUnit_Framework_TestCase
+class BerlinClockKataTest extends \PHPUnit\Framework\TestCase
 {
-
+    public function testMinutesGiven1(){
+        $berlinclock = new BerlinClockKata();
+        $actual = $berlinclock->getMinute(1);
+        $this->assertEquals("OOOOOOOOOOO YOOO",$actual);
+    }
 }
